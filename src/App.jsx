5085cdfe7c788594052aa0864f8cd64a7936ac20
bplacke5 <braxtonplacke@gmail.com>
2026-02-04 16:12:50 -0600

    Fixed formatting

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,24 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="list">
+      <>
+          <div className="center-wrapper">
+              <div className="h1">
+                  Braxton's Links
+          </div>
+          <div className="list">
+
               <a href="https://github.com/BraxtonPlacke" target="_blank" rel="noreferrer" aria-label="GitHub profile">
                   <ImGithub />
               </a>
               <a href="https://www.linkedin.com/in/braxton-placke-290917306" target="_blank" rel="noreferrer" aria-label="LinkedIn profile">
                   <FaLinkedin />
-              </a>
-      </div>
+                  </a>
+                  <a href="https://www.linkedin.com/in/braxton-placke-290917306">
+                
+                  </a>
+              </div>
+          </div>
     </>
   )
 }
