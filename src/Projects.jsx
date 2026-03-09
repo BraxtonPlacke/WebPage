86db6fbe0ccb3a4af5b9c4207f79d90a59e5037a
bplacke5 <braxtonplacke@gmail.com>
2026-03-08 19:19:31 -0500

    Updated project description

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -17,7 +17,8 @@ function Projects() {
                             />
                             <p>A REST API built with ASP.NET Core that serves country and flag data to a React frontend,
                                 powering an interactive flag guessing quiz.
-                                Data is pulled from a SQL Server database hosted on Azure.</p>
+                                Data is pulled from a SQL Server database hosted on Azure.
+                                May take a minute to load because it is hosted on Azure's shared infrastructure.</p>
                         </div>
                     </div>
                 </Link>
